a3b8fc89d3c7a3bc376c82801188becb97615da3
Misha Aizatulin <avatar@hot.ee>
2017-02-19 14:37:24 +0000

    fix license

--- atoms/index_client_light.ml
+++ atoms/index_client_light.ml
@@ -10,7 +10,10 @@ open Common
 
 (* CR: on some phones touch doesn't work *)
 
-(* CR; on Chris' phone it keeps reloading in landscape mode. *)
+(* CR; on Chris' phone it keeps reloading in landscape mode.
+   Have a look at
+   http://stackoverflow.com/questions/1649086/detect-rotation-of-android-phone-in-the-browser-with-javascript
+*)
 
 (* CR; Chris said it would be nice to control the decay. *)
 
