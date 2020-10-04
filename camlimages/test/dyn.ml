fbbf4b08dd6c7fd9534e38e396f9a3096f9bda8f
tari3x <avatar@hot.ee>
2020-10-04 18:57:34 +0100

    camlimages

--- camlimages/test/dyn.ml
+++
@@ -1,7 +0,0 @@
-let () =
-  Dynlink.allow_unsafe_modules true; (* for bytecode *)
-  Dynlink.loadfile
-    (if Dynlink.is_native then "../src/camlimages_jpeg.cmxs"
-     else "../src/camlimages_jpeg.cma");
-  let i = Images.load "images/jpg.jpg" [] in
-  Images.save "out-dyn.bmp" None [] i
