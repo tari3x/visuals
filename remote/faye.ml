d2dabf659957531d971c2127f8355c26c8e4e129
tari3x <avatar@hot.ee>
2020-10-09 19:02:45 +0100

    faye

--- remote/faye.ml
+++ remote/faye.ml
@@ -31,42 +31,50 @@ module Channel = struct
   include String
 
   let global s = sprintf "/%s" s
-  let create () =
-    Printf.sprintf "/%d" (Random.int 100_000_000)
+  let create () = Printf.sprintf "/%d" (Random.int 100_000_000)
 end
 
 (* CR: try publishing without converting *)
-class type faye = object
-  method subscribe : js_string Js.t -> (js_string Js.t -> unit) callback -> unit meth
-  method publish : js_string Js.t -> js_string Js.t -> unit meth
-end
+class type faye =
+  object
+    method subscribe :
+      js_string Js.t -> (js_string Js.t -> unit) callback -> unit meth
+
+    method publish : js_string Js.t -> js_string Js.t -> unit meth
+  end
 
 type 'a t =
   { faye : faye Js.t
-  ; sexp_of_a : ('a -> Sexp.t)
+  ; sexp_of_a : 'a -> Sexp.t
   ; buffers : 'a Ordered_stream.t Hashtbl.M(Channel).t
   }
 
 let constr : (js_string Js.t -> faye Js.t) constr =
   Js.Unsafe.global##._Faye##._Client
-
-let faye_url =
-  sprintf "http://%s:8000/faye" (to_string (Html.window##.location##.hostname))
+;;
 
 let create ~sexp_of_a =
-  let faye = new%js constr (string faye_url) in
+  let url =
+    sprintf
+      "http://%s:8000/faye"
+      (to_string Html.window##.location##.hostname)
+  in
+  let faye = new%js constr (string url) in
   let buffers = Hashtbl.create (module Channel) in
   { faye; sexp_of_a; buffers }
+;;
 
 let get_buffer t channel =
   Hashtbl.find_or_add t.buffers channel ~default:(fun () ->
-    Ordered_stream.create ~max_buffer_size:5)
+      Ordered_stream.create ~max_buffer_size:5)
+;;
 
 let publish (t : 'a t) channel msg =
   let buffer = get_buffer t channel in
   (* debug "Publishing %s on %s" (Message.to_string msg) channel; *)
   let msg = Ordered_stream.create_element buffer msg in
   t.faye##publish (string channel) (Json.output msg)
+;;
 
 let subscribe_with_try (t : 'a t) channel ~f =
   let buffer = get_buffer t channel in
@@ -80,8 +88,9 @@ let subscribe_with_try (t : 'a t) channel ~f =
     f msg
   in
   (* debug "Subscribed to %s" channel; *)
-  t.faye##subscribe
-    (string channel)
-    (Js.wrap_callback write_to_buffer);
+  t.faye##subscribe (string channel) (Js.wrap_callback write_to_buffer);
   Lwt.async (fun () ->
-    Lwt_stream.iter_with_try (Ordered_stream.reader buffer) ~f:read_from_buffer)
+      Lwt_stream.iter_with_try
+        (Ordered_stream.reader buffer)
+        ~f:read_from_buffer)
+;;
