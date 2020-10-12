0162874552e0cceb6ca4c6bdbcd71dff60005343
tari3x <avatar@hot.ee>
2020-10-12 19:11:25 +0100

    rm

--- spark/lib/server.ml
+++ spark/lib/server.ml
@@ -66,15 +66,14 @@ let main (config : Config.t) =
   Config.validate config;
   Random.self_init ();
   let%bind sound =
-    (* CR: undo or do something more snesible *)
+    (*
     let%bind () = Lwt_js.sleep 3. in
     Sound.create_from_html
       ~id:"audio"
       ~max_sources:(Config.num_sound_sources config)
     |> return
-    (*
-       Sound.create_from_mic ~max_sources:(Config.num_sound_sources config)
     *)
+    Sound.create_from_mic ~max_sources:(Config.num_sound_sources config)
   in
   (*
   let sound =
